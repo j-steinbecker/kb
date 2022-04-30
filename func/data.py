9d81eea583c47c956d95a27fee72081cea6ec4d4
j-steinbecker <104669816+j-steinbecker@users.noreply.github.com>
2022-04-30 05:52:36 -0600

    Update data.py

--- func/data.py
+++ func/data.py
@@ -22,3 +22,36 @@ def ymlFlattenDates (data):
                   for day,shift in val.items():
                         dic.update({str(yr)+"-"+str(mo)+"-"+str(day): shift})
       return dic
+
+
+
+def accumulate (Set, specialReturn= None) -> list:
+   """_summary_
+            Running Total Accumulation 
+            of List of numbers
+
+      >>> Set = [2, 3,  7, 12, 14, 16, 2]
+      >>> accumulate(Set)
+                [2, 5, 12, 24, 38, 54, 56]
+   ---------------------------------------
+   """
+   accum = [0]
+   
+   for x in Set:
+      new = accum[-1] + x
+      accum.append(new)
+      
+   if specialReturn == 'md':
+      md_str = ""
+      for x in range(len(Set)):
+         ob = '{'; cb = '}'
+         md_str += f'\\xrightarrow{ob}+{Set[x]}{cb} {accum[x+1]}'
+      
+      md = "$" + md_str + "$"
+      
+      return Markdown(md)
+   else:
+      pass
+   
+   if specialReturn == None:
+      return accum[1:] 
