16ddf9822eb25bb028da376c15bc85283b9b7aec
j-steinbecker <jtsteinbecker@gmail.com>
2022-04-30 08:27:55 +0000

    alligate

---
+++ func/alligate.py
@@ -0,0 +1,32 @@
+def Alligate (Drug, higherConc, lowerConc, desiredConc, Amount):
+      """
+      |=Alligate========================|
+      |    Drug  :          str
+      |    higherConc       [0-1]
+      |    lowerConc        [0-1]
+      |    desiredConc      [0-1]
+      |    Amount (mL) :    INT
+      |=================================|
+      """
+
+      hc = higherConc ; lc = lowerConc ; dc = desiredConc ; A = Amount
+
+      numer = dc - lc #"Parts-per" of the higher concentration stock
+      denom = hc - dc #"Parts-per" of the  lower concentration stock
+
+      U = round(A * numer / (numer + denom),2)
+      L = round(A * denom / (numer + denom),2)
+
+
+      #  [hi]\    /numer
+      #       [dc]           numer : denom = (parts per)
+      #  [lc]/    \denom
+
+      out =  f'RECEPIE FOR\n{Amount}mL of {desiredConc*100}% {Drug}\n{"-"*45}'
+      out += f'\n\n\t+ {U}mL of {round(hc*100,2)}% {Drug}'
+      out += f'\n\n\t+ {L}mL of {round(lc*100,2)}% {Drug}'
+      out += f'\n\n{"-"*45}'
+      out += f'\n\\t   {Amount}mL of {round(dc*100,2)}% {Drug}'
+      
+      
+      print (out)
