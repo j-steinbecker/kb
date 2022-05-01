532726c33f13c4aa4a000bed5ac6c8715cf7d62f
j-steinbecker <104669816+j-steinbecker@users.noreply.github.com>
2022-04-30 20:25:29 -0600

    Create finance.py

---
+++ finance.py
@@ -0,0 +1,21 @@
+def monthlypayment (P, apr, yrs):
+   """
+   ----MORTGAGE PAYMENT-----------------------------------
+   Returns MONTHLY PAYMENT IN $
+   -------------------------------------------------------
+   from args:
+      >>> P     : principle
+      >>> apr   : decimal APR
+      >>> yrs   : length of term in years
+   ======================================================
+   """
+      
+   r = apr / 12       # MONTHLY PERCENTAGE RATE
+   n = yrs * 12       # NUMBER OF MONTHS
+   
+   # MONTHLY PAYMENT
+   mp = round(P * r * ((r+1)**n) / ((r+1)**n-1) , 2)
+
+   return mp
+  
+   #-----------------------------------------------------
